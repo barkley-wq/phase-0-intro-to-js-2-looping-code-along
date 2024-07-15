17d14fb6d5b3cd32d26277b8feb0c796b5c4205e
barkley-wq <igonyibarkley00@gmail.com>
2024-07-15 19:46:54 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ['ben', 'james','luke'];
+function writeCards(names, gift = 'surprise'){
+    let message = [];
+    for(let i= 0 ; i < names.length; i++){
+        message.push(`Thank you, ${names[i]}, for the wonderful ${gift} gift!`)
+    }
+    return message;
+}
+//console.log(writeCards(names));
+
+function countDown(number){
+    while(number >= 0){
+        console.log(number)
+        number--
+    }
+}
+countDown(11)
